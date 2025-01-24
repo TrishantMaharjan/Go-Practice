45554b731aee50a787c620c0ec95740fdf79e0a4
Trishant Maharjan <trishant0002@gmail.com>
2025-01-24 13:18:49 +0545

    CHANGED: ex5's name, tried tour of go 1-3

---
+++ ex5_tour_of_go/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	// list of packages
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	const name, age = "Kim", 22
+	fmt.Println(name, "is", age, "years old") // function is defined using parameters and used with arguments
+
+	fmt.Println("The time is", time.Now())
+
+	fmt.Println("My favorite number is", rand.Intn(6)) // Random number from 0 to 5, [0,n) -> inclusive of 0, exclusive of n
+
+	fmt.Printf("Now you have %v problems \n", math.Sqrt(9))
+
+	fmt.Println(math.Pi) // In go, a name is exported if it begins with a capital letter. Here, Pi is a constant, so () is not used. () is used to execute a function
+}
