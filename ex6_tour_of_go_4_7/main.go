3565a4ecb608401ddce6e96db4e794f03a06966d
Trishant Maharjan <trishant0002@gmail.com>
2025-01-31 19:42:28 +0545

    CHANGED: Name of exercise 5 to include go exercise number ADDED: Exercise 6 with basics of functions

---
+++ ex6_tour_of_go_4_7/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func add(x, y int) int { // Here, x and y share a same type so add(x int, y int) was shortned to add(x, y int)
+	return x + y
+}
+
+func sayHello() {
+	fmt.Println("Hello!!")
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func split(sum int) (x, y int) { // Named return values, this should be used only in short functions as they can harm readability in longer functions
+	x = sum % 10
+	y = sum - x
+	return // no need to return here if returned when initializing the function
+}
+
+func main() {
+	fmt.Println(add(42, 13))
+	sayHello()
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+	fmt.Println(split(10))
+}
