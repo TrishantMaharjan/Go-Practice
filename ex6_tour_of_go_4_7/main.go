7ee60cec999ecf00f5cc2dd8f0470aee5e5c39ce
Trishant Maharjan <trishant0002@gmail.com>
2025-01-31 19:51:23 +0545

    ADDED: comments to ex6

--- ex6_tour_of_go_4_7/main.go
+++ ex6_tour_of_go_4_7/main.go
@@ -16,7 +16,7 @@ func swap(x, y string) (string, string) {
 	return y, x
 }
 
-func split(sum int) (x, y int) { // Named return values, this should be used only in short functions as they can harm readability in longer functions
+func split(sum int) /* Parameters */ (x, y int) { // Named return values, this should be used only in short functions as they can harm readability in longer functions
 	x = sum % 10
 	y = sum - x
 	return // no need to return here if returned when initializing the function
@@ -27,5 +27,5 @@ func main() {
 	sayHello()
 	a, b := swap("hello", "world")
 	fmt.Println(a, b)
-	fmt.Println(split(10))
+	fmt.Println(split(10)) // Arguments
 }
